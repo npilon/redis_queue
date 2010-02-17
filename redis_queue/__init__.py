7414e2fd3074db6d7a378a61f7fd52e6bd9ddf66
Nick Pilon <npilon@oreilly.com>
2010-02-16 16:42:48 -0800

    Redis-based Queue, ExclusiveQueue, and tests.

--- redis_queue/__init__.py
+++ redis_queue/__init__.py
@@ -1,1 +1,99 @@
 #
+
+import redis
+
+class Queue(object):
+    """Implements the same interface as collections.dequeue"""
+    def __init__(self, redis, queue_name):
+        self._redis = redis
+        self.queue_name = queue_name
+    
+    def append(self, x):
+        self._redis.push(self.queue_name, x)
+    
+    def appendleft(self, x):
+        self._redis.push(self.queue_name, x, head=True)
+    
+    def clear(self):
+        self._redis.delete(self.queue_name)
+    
+    def extend(self, iterable):
+        """Non-atomic. append each of the items from iterable in turn."""
+        for item in iterable:
+            self.append(item)
+    
+    def extendleft(self, iterable):
+        """Non-atomic. appendleft each of the items from iterable in turn."""
+        for item in iterable:
+            self.appendleft(item)
+    
+    def pop(self):
+        item = self._redis.pop(self.queue_name, tail=True)
+        if item is None:
+            raise IndexError
+        return item
+    
+    def popleft(self):
+        item = self._redis.pop(self.queue_name)
+        if item is None:
+            raise IndexError
+        return item
+    
+    def remove(self, value):
+        num_removed = self._redis.lrem(self.queue_name, value)
+        if num_removed == 0:
+            raise ValueError()
+    
+    def rotate(self):
+        """rotates the queue one step only."""
+        self_redis.poppush(self.queue_name, self.queue_name)
+        
+    # Membership
+    def __contains__(self, item):
+        # This could stand to be a lot more efficient.
+        # Why doesn't redis support this on the server side?
+        members = self._redis.lrange(self.queue_name, 0, -1)
+        return item in members
+    
+    # Length
+    def __len__(self):
+        return self._redis.llen(self.queue_name)
+    
+    # Subscript
+    def __getitem__(self, key):
+        item = self._redis.lindex(self.queue_name, key)
+        if item is None:
+            raise IndexError
+        return item
+    
+    def __setitem__(self, key, value):
+        try:
+            self._redis.lset(self.queue_name, key, value)
+        except redis.ResponseError, e:
+            raise IndexError(e)
+    
+    # Iteration
+    def __iter__(self):
+        members = self._redis.lrange(self.queue_name, 0, -1)
+        return iter(members)
+
+class ExclusiveQueue(Queue):
+    """A queue that implements exclusivity (a given thing can only be in the
+    queue once. This is not at all atomic; it pops something off then removes
+    all other instances from the queue."""
+    
+    def pop(self):
+        item = super(ExclusiveQueue, self).pop()
+        try:
+            self.remove(item)
+        except ValueError:
+            pass
+        return item
+    
+    def popleft(self):
+        item = super(ExclusiveQueue, self).popleft()
+        try:
+            self.remove(item)
+        except ValueError:
+            pass
+        return item
---
+++ redis_queue/tests/test_functional.py
@@ -0,0 +1,159 @@
+import unittest
+import redis
+from redis_queue import Queue, ExclusiveQueue
+
+class TestRedisQueue(unittest.TestCase):
+    def setUp(self):
+        self.redis = redis.Redis(host='127.0.0.1', port=6379)
+        self.queue_name = 'test_redis_queue'
+        self.queue = Queue(self.redis, self.queue_name)
+    
+    def tearDown(self):
+        self.redis.delete(self.queue_name)
+    
+    def test_right(self):
+        self.queue.append('one')
+        self.queue.append('two')
+        self.queue.append('three')
+        self.assertEqual(self.queue.pop(), 'three')
+        self.assertEqual(self.queue.pop(), 'two')
+        self.assertEqual(self.queue.pop(), 'one')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_left(self):
+        self.queue.appendleft('one')
+        self.queue.appendleft('two')
+        self.queue.appendleft('three')
+        self.assertEqual(self.queue.popleft(), 'three')
+        self.assertEqual(self.queue.popleft(), 'two')
+        self.assertEqual(self.queue.popleft(), 'one')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_right_left(self):
+        self.queue.append('one')
+        self.queue.append('two')
+        self.queue.append('three')
+        self.assertEqual(self.queue.popleft(), 'one')
+        self.assertEqual(self.queue.popleft(), 'two')
+        self.assertEqual(self.queue.popleft(), 'three')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_left_right(self):
+        self.queue.appendleft('one')
+        self.queue.appendleft('two')
+        self.queue.appendleft('three')
+        self.assertEqual(self.queue.pop(), 'one')
+        self.assertEqual(self.queue.pop(), 'two')
+        self.assertEqual(self.queue.pop(), 'three')
+        self.assertRaises(IndexError, self.queue.pop)
+
+    def test_clear(self):
+        self.queue.appendleft('one')
+        self.queue.appendleft('two')
+        self.queue.appendleft('three')
+        self.queue.clear()
+        self.assertRaises(IndexError, self.queue.pop)
+        self.assertRaises(IndexError, self.queue.popleft)
+    
+    def test_remove(self):
+        self.queue.append('one')
+        self.queue.append('two')
+        self.queue.append('two')
+        self.queue.append('three')
+        self.queue.append('two')
+        self.queue.remove('two')
+        self.assertEqual(self.queue.pop(), 'three')
+        self.assertEqual(self.queue.pop(), 'one')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_contains(self):
+        self.queue.append('one')
+        self.queue.append('two')
+        self.queue.append('three')
+        self.assert_('one' in self.queue)
+        self.assert_('two' in self.queue)
+        self.assert_('three' in self.queue)
+        self.assert_('four' not in self.queue)
+    
+    def test_extend_right(self):
+        self.queue.extend(['one', 'two', 'three'])
+        self.assertEqual(self.queue.pop(), 'three')
+        self.assertEqual(self.queue.pop(), 'two')
+        self.assertEqual(self.queue.pop(), 'one')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_extend_left(self):
+        self.queue.extendleft(['one', 'two', 'three'])
+        self.assertEqual(self.queue.popleft(), 'three')
+        self.assertEqual(self.queue.popleft(), 'two')
+        self.assertEqual(self.queue.popleft(), 'one')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_iteration(self):
+        self.queue.append('one')
+        self.queue.append('two')
+        self.queue.append('three')
+        self.assertEqual(list(iter(self.queue)), ['one', 'two', 'three'])
+    
+    def test_length(self):
+        self.queue.append('one')
+        self.queue.append('two')
+        self.queue.append('three')
+        self.assertEqual(len(self.queue), 3)
+
+class TestRedisExclusiveQueue(unittest.TestCase):
+    def setUp(self):
+        self.redis = redis.Redis(host='127.0.0.1', port=6379)
+        self.queue_name = 'test_redis_queue'
+        self.queue = ExclusiveQueue(self.redis, self.queue_name)
+    
+    def tearDown(self):
+        self.redis.delete(self.queue_name)
+    
+    def test_right(self):
+        self.queue.append('two')
+        self.queue.append('two')
+        self.queue.append('two')
+        self.queue.append('one')
+        self.queue.append('two')
+        self.queue.append('three')
+        self.assertEqual(self.queue.pop(), 'three')
+        self.assertEqual(self.queue.pop(), 'two')
+        self.assertEqual(self.queue.pop(), 'one')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_left(self):
+        self.queue.appendleft('two')
+        self.queue.appendleft('two')
+        self.queue.appendleft('two')
+        self.queue.appendleft('one')
+        self.queue.appendleft('two')
+        self.queue.appendleft('three')
+        self.assertEqual(self.queue.popleft(), 'three')
+        self.assertEqual(self.queue.popleft(), 'two')
+        self.assertEqual(self.queue.popleft(), 'one')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_right_left(self):
+        self.queue.append('one')
+        self.queue.append('two')
+        self.queue.append('three')
+        self.queue.append('two')
+        self.queue.append('two')
+        self.queue.append('two')
+        self.assertEqual(self.queue.popleft(), 'one')
+        self.assertEqual(self.queue.popleft(), 'two')
+        self.assertEqual(self.queue.popleft(), 'three')
+        self.assertRaises(IndexError, self.queue.pop)
+    
+    def test_left_right(self):
+        self.queue.appendleft('one')
+        self.queue.appendleft('two')
+        self.queue.appendleft('three')
+        self.queue.appendleft('two')
+        self.queue.appendleft('two')
+        self.queue.appendleft('two')
+        self.assertEqual(self.queue.pop(), 'one')
+        self.assertEqual(self.queue.pop(), 'two')
+        self.assertEqual(self.queue.pop(), 'three')
+        self.assertRaises(IndexError, self.queue.pop)
